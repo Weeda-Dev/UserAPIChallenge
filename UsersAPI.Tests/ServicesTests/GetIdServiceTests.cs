e2981f581237b7d9f5134559e003e3cb078fb13c
Weeda Lostitmonton <weeda.dev@gmail.com>
2020-04-24 17:02:17 +1200

    Started unit testing

---
+++ UsersAPI.Tests/ServicesTests/GetIdServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using Shouldly;
+using System.Collections.Generic;
+using UsersAPI.Utilities;
+
+namespace UsersAPI.Tests.ServicesTests
+{
+    [TestFixture]
+    public class GetIdServiceTests
+    {
+        [TestCase(34)]
+        [TestCase(1)]
+        [TestCase(1845854)]
+        [TestCase(98563253)]
+        public void GivenValidUserModel_WhenGetNewUserIdIsCalled_TheShouldReturnCorrectNewUserId(int lastIdOnUsersList)
+        {
+            //Set up
+            IList<UserModel> newUser = new List<UserModel>();
+            newUser.Add(new UserModel { Id = lastIdOnUsersList });
+            var mock = new Mock<IEnumerable<UserModel>>();
+            mock.Setup(x => x.GetEnumerator()).Returns(newUser.GetEnumerator());
+            GetIdService getIdServ = new GetIdService();
+
+            var expectedResult = lastIdOnUsersList + 1;
+            var result = getIdServ.GetNewUserId(mock.Object);
+
+            result.ShouldBe(expectedResult);
+            result.ShouldNotBe(lastIdOnUsersList);
+
+        }
+    }
+}
