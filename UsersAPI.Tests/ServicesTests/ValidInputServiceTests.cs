32297f4c62c23556b5351f42674a886890f63969
Weeda Lostitmonton <weeda.dev@gmail.com>
2020-04-24 23:18:10 +1200

    Added test for ValidInputService

---
+++ UsersAPI.Tests/ServicesTests/ValidInputServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using UsersAPI.Utilities;
+
+namespace UsersAPI.Tests.ServicesTests
+{
+    [TestFixture]
+    public class ValidInputServiceTests
+    {
+        [Test]
+        public void GivenNullFirstNameAndLastName_WhenTurnNullToEmptyString_ShouldReturnEmptyString()
+        {
+            //Setup
+            string firstName = null;
+            string lastName = null;
+
+            //Act
+            ValidInputService vS = new ValidInputService();
+            vS.TurnNullToEmptyString(ref firstName, ref lastName);
+
+            //Assert
+            firstName.ShouldBe("");
+            lastName.ShouldBe("");
+            firstName.ShouldNotBeNull();
+            lastName.ShouldNotBeNull();
+        }
+
+        [Test]
+        public void GivenValidFirstNameAndLastName_WhenTurnNullToEmptyString_ShouldReturnValidFirstNameandLastName()
+        {
+            //Setup
+            var firstName = "John";
+            var lastName = "Smith";
+
+            //Act
+            ValidInputService vS = new ValidInputService();
+            vS.TurnNullToEmptyString(ref firstName, ref lastName);
+
+            //Assert
+            firstName.ShouldBe("John");
+            lastName.ShouldBe("Smith");
+            firstName.ShouldNotBe("");
+            lastName.ShouldNotBe("");
+        }
+
+        [Test]
+        public void GivenValidFirstNameAndNullLastName_WhenTurnNullToEmptyString_ShouldReturnFirstNameAndLastNameAsEmptyString()
+        {
+            //Setup
+            string firstName = "John";
+            string lastName = null;
+
+            //Act
+            ValidInputService vS = new ValidInputService();
+            vS.TurnNullToEmptyString(ref firstName, ref lastName);
+
+            //Assert
+            firstName.ShouldBe("John");
+            firstName.ShouldNotBe("");
+            lastName.ShouldBe("");
+            lastName.ShouldNotBe(null);
+        }
+
+        [Test]
+        public void GivenNullFirstNameAndValidLastName_WhenTurnNullToEmptyString_ShouldReturnFirstNameAsEmptyStringAndLastName()
+        {
+            //Setup
+            string firstName = null;
+            string lastName = "Smith";
+
+            //Act
+            ValidInputService vS = new ValidInputService();
+            vS.TurnNullToEmptyString(ref firstName, ref lastName);
+
+            //Assert
+            firstName.ShouldBe("");
+            firstName.ShouldNotBe(null);
+            lastName.ShouldBe("Smith");
+            lastName.ShouldNotBe("");
+        }
+    }
+}
