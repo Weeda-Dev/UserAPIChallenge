4222543b54ed8448a057e0b64e6786428a47271a
Weeda Lostitmonton <weeda.dev@gmail.com>
2020-04-25 15:36:06 +1200

    Added another test to test another method on GetUsersServiceTests

--- UsersAPI.Tests/ServicesTests/GetUsersServiceTests.cs
+++ UsersAPI.Tests/ServicesTests/GetUsersServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
 using Shouldly;
+using System.Collections.Generic;
 using System.Linq;
 using UsersAPI.Interfaces;
 using UsersAPI.Tests.Utilities;
@@ -12,27 +13,52 @@ namespace UsersAPI.Tests.ServicesTests
     public class GetUsersServiceTests
     {
         [Test]
-        public void WhenGetUserListRootObject_ShouldReturnValidRootObjectList()
+        public void WhenGetUserLists_ShouldReturnValidUsersList()
         {
             //Set up
-            var mockJsonHelper = new Mock<IGetJsonFileDataService>();
-            var fakejson = FakeJsonHelper.GetFakeValidUserDataJson();
-            mockJsonHelper.Setup(x => x.GetUsersDataFromJsonFile())
-                .Returns(fakejson);
+            Mock<IGetJsonFileDataService> mockJsonHelper = GetMockJsonHelperSetUp();
 
             //Act
             GetUsersService gUserServ = new GetUsersService(mockJsonHelper.Object);
-            var userListRootObject = gUserServ.GetUserListRootObject();
-            var expectedItemValidCount = userListRootObject.users.Count() == 7;
+            var userList = gUserServ.GetUserLists();
+            var expectedItemValidCount = userList.Count() == 7;
 
-            var allUsers = userListRootObject.users;
-            var actualFirstPersonFirstName = allUsers.FirstOrDefault().FirstName;
+            var actualFirstPersonFirstName = userList.FirstOrDefault().FirstName;
             var expectedFirstPersonFirstName = "brad";
 
             //Assert
             Assert.True(expectedItemValidCount, "Item count does not match the expected count (7).");
             actualFirstPersonFirstName.ShouldBe(expectedFirstPersonFirstName, "First name of the first person " +
                 $"on the list is not {expectedFirstPersonFirstName}.");
+        }
+
+        [Test]
+        public void WhenGetUserListRootObject_ShouldReturnValidRootObjectList()
+        {
+            //Set up
+            Mock<IGetJsonFileDataService> mockJsonHelper = GetMockJsonHelperSetUp();
+
+            //Act
+            GetUsersService gUserServ = new GetUsersService(mockJsonHelper.Object);
+            AllUsersModel userListRootObject = gUserServ.GetUserListRootObject();
+            bool expectedItemValidCount = userListRootObject.users.Count() == 7;
+
+            IEnumerable<UserModel> allUsers = userListRootObject.users;
+            string actualFirstPersonFirstName = allUsers.FirstOrDefault().FirstName;
+            string expectedFirstPersonFirstName = "brad";
+
+            //Assert
+            Assert.True(expectedItemValidCount, "Item count does not match the expected count (7).");
+            actualFirstPersonFirstName.ShouldBe(expectedFirstPersonFirstName, "First name of the first person " +
+                $"on the list is not {expectedFirstPersonFirstName}.");
+        }
+        private static Mock<IGetJsonFileDataService> GetMockJsonHelperSetUp()
+        {
+            var mockJsonHelper = new Mock<IGetJsonFileDataService>();
+            var fakejson = FakeJsonHelper.GetFakeValidUserDataJson();
+            mockJsonHelper.Setup(x => x.GetUsersDataFromJsonFile())
+                .Returns(fakejson);
+            return mockJsonHelper;
         }
     }
 }
