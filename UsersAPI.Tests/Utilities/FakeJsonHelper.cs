1d6e0b129a38be2abfff795f648aa9f3f3e10cb4
Weeda Lostitmonton <weeda.dev@gmail.com>
2020-04-25 15:10:37 +1200

    Moved FakeJson file in test to a separated file

---
+++ UsersAPI.Tests/Utilities/FakeJsonHelper.cs
@@ -0,0 +1,90 @@
+﻿namespace UsersAPI.Tests.Utilities
+{
+    public static class FakeJsonHelper
+    {
+        public static string GetFakeValidUserDataJson()
+        {
+            return @"
+{
+'users':[
+{
+      'Id': 0,
+      'Title': 'mr',
+      'FirstName': 'brad',
+      'LastName': 'gibson',
+      'Email': 'brad.gibson@example.com',
+      'Birthday': '1993-07-20',
+      'MobileNumber': '011-962-7516',
+      'ProfileImageLarge': 'https://randomuser.me/api/portraits/men/75.jpg',
+      'ProfileImageThumbnail': 'https://randomuser.me/api/portraits/med/men/75.jpg'
+    },
+{
+      'Id': 1,
+      'Title': 'mrs',
+      'FirstName': 'gomel',
+      'LastName': 'tun',
+      'Email': 'gomel.tun@example.com',
+      'Birthday': '1995-09-10',
+      'MobileNumber': '011-962-7516',
+      'ProfileImageLarge': 'https://randomuser.me/api/portraits/men/75.jpg',
+      'ProfileImageThumbnail': 'https://randomuser.me/api/portraits/med/men/75.jpg'
+    },
+{
+      'Id': 2,
+      'Title': 'mr',
+      'FirstName': 'brad',
+      'LastName': 'gibson',
+      'Email': 'brad.gibson@example.com',
+      'Birthday': '1993-07-20',
+      'MobileNumber': '011-962-7516',
+      'ProfileImageLarge': 'https://randomuser.me/api/portraits/men/75.jpg',
+      'ProfileImageThumbnail': 'https://randomuser.me/api/portraits/med/men/75.jpg'
+    },
+{
+      'Id': 3,
+      'Title': 'miss',
+      'FirstName': 'steph',
+      'LastName': 'suan',
+      'Email': 'steph.suan@example.com',
+      'Birthday': '1993-07-20',
+      'MobileNumber': '011-962-7516',
+      'ProfileImageLarge': 'https://randomuser.me/api/portraits/men/75.jpg',
+      'ProfileImageThumbnail': 'https://randomuser.me/api/portraits/med/men/75.jpg'
+    },
+{
+      'Id': 4,
+      'Title': 'miss',
+      'FirstName': 'gamel',
+      'LastName': 'tele',
+      'Email': 'gamel.tele@example.com',
+      'Birthday': '1993-07-20',
+      'MobileNumber': '011-962-7516',
+      'ProfileImageLarge': 'https://randomuser.me/api/portraits/men/75.jpg',
+      'ProfileImageThumbnail': 'https://randomuser.me/api/portraits/med/men/75.jpg'
+    },
+{
+      'Id': 5,
+      'Title': 'mr',
+      'FirstName': 'grat',
+      'LastName': 'gibs',
+      'Email': 'grat.gibs@example.com',
+      'Birthday': '1993-07-20',
+      'MobileNumber': '011-962-7516',
+      'ProfileImageLarge': 'https://randomuser.me/api/portraits/men/75.jpg',
+      'ProfileImageThumbnail': 'https://randomuser.me/api/portraits/med/men/75.jpg'
+    },
+{
+      'Id': 6,
+      'Title': 'mr',
+      'FirstName': 'wat',
+      'LastName': 'riv',
+      'Email': 'wat.riv@example.com',
+      'Birthday': '1993-07-20',
+      'MobileNumber': '011-962-7516',
+      'ProfileImageLarge': 'https://randomuser.me/api/portraits/men/75.jpg',
+      'ProfileImageThumbnail': 'https://randomuser.me/api/portraits/med/men/75.jpg'
+    }
+]}";
+        }
+    }
+}
