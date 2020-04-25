1335b0f260720ad3c94c580c65ea29be4e948b58
Weeda Lostitmonton <weeda.dev@gmail.com>
2020-04-25 15:17:00 +1200

    Simplified GetIdServiceTests test

--- UsersAPI.Tests/ServicesTests/GetIdServiceTests.cs
+++ UsersAPI.Tests/ServicesTests/GetIdServiceTests.cs
@@ -7,18 +7,16 @@ namespace UsersAPI.Tests.ServicesTests
     [TestFixture]
     public class GetIdServiceTests
     {
-        [TestCase(34)]
-        [TestCase(1)]
-        [TestCase(1845854)]
-        [TestCase(98563253)]
-        public void GivenValidUserModel_WhenGetNewUserIdIsCalled_TheShouldReturnCorrectNewUserId(int lastIdOnUsersList)
+        [TestCase(34,35)]
+        [TestCase(1,2)]
+        [TestCase(1845854,1845855)]
+        [TestCase(98563253,98563254)]
+        public void GivenLastIdOnUsersList_WhenGetNewUserIdIsCalled_ShouldReturnCorrectNewUserId(int lastIdOnUsersList, int expectedNewUserId)
         {
             GetIdService getIdServ = new GetIdService();
-
-            var expectedResult = lastIdOnUsersList + 1;
             var result = getIdServ.GetNewUserId(lastIdOnUsersList);
 
-            result.ShouldBe(expectedResult);
+            result.ShouldBe(expectedNewUserId);
         }
     }
 }
